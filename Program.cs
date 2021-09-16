7996e8554c61ee2caabf5a90252040490f3c6e54
kaiorod <caiorodrigo2775@gmail.com>
2021-09-16 00:50:29 -0300

    Finaliza FGTS

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, fgts;
+
+            Console.Clear();
+            Console.Beep();
+
+            Console.Write("Digite o salário : ");
+            sal = double.Parse(Console.ReadLine());
+
+            fgts = sal * 0.08;
+
+            Console.Clear();
+            Console.WriteLine($"Salário : {sal:C2}");
+            Console.Write($"FGTS : ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{fgts:C2}");
+            Console.ResetColor();
+        }
+    }
+}
